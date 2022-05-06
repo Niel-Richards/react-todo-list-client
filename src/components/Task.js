b16753efa6f9cd5a720ba817bed73ea422ca5b3d
O'Niel Richards <92700558+Niel-Richards@users.noreply.github.com>
2022-05-06 02:52:09 +0100

    Added box-shadow to wrappers, reduced border on add button by 1px, Centered and increased the size of the icons on either side of the task

--- src/components/Task.js
+++ src/components/Task.js
@@ -2,13 +2,13 @@ import {FaRegTrashAlt, FaRegSquare, FaRegCheckSquare } from 'react-icons/fa'
 function Task({task, onToggle, onDelete}) {
     return (
         <div className="Task__wrapper">
-            <div className="Task__tickBox" onClick={() => onToggle(task.id)}>
-                {task.isComplete ?<FaRegCheckSquare style={{ color: '#5cb85c', cursor: 'pointer'}}/>:<FaRegSquare style={{ color: '#1DA1F2', cursor: 'pointer'}}/>}
+            <div className="Task__tickBox center" onClick={() => onToggle(task.id)}>
+                {task.isComplete ?<FaRegCheckSquare style={{ color: '#5cb85c', cursor: 'pointer', fontSize:'inherit'}}/>:<FaRegSquare style={{ color: '#1DA1F2', cursor: 'pointer', fontSize:'inherit'}}/>}
             </div>
             <p className={task.isComplete ? "Task__desc Task__desc--complete":"Task__desc"}>{task.description}</p>
             <p className="Task__date">{task.entryDate}</p>
-            <div className="Task__delete" onClick={()=>onDelete(task.id)}>
-                <FaRegTrashAlt style={{ color: 'red', cursor: 'pointer'}}/>
+            <div className="Task__delete center" onClick={()=>onDelete(task.id)}>
+                <FaRegTrashAlt style={{ color: 'red', cursor: 'pointer', fontSize:'inherit'}}/>
             </div>
         </div>
     );
